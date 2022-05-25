2d6572d23a44e33c3de2c50e9560b3e07d6e4492
Sakshi Singh <sakshisingh291999@gmail.com>
2022-05-25 13:16:58 +0530

    First Commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,36 @@
+import processing.core.PApplet;
+
+
+public class MainClass extends PApplet{
+
+    public static final float DIAMETER =21;
+    public static final float HEIGHT =480;
+    float x1=0,x2=0,x3=0,x4=0;
+
+
+    public static void main(String args[]){
+        PApplet.main("MainClass",args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(680,480);
+    }
+
+    @Override
+    public void setup() {
+        background(255);
+
+    }
+
+    @Override
+    public void draw() {
+        ellipse(x1+=1,HEIGHT/5  , DIAMETER, DIAMETER);
+        ellipse(x2+=2,HEIGHT*2/5, DIAMETER, DIAMETER);
+        ellipse(x3+=3,HEIGHT*3/5, DIAMETER, DIAMETER);
+        ellipse(x4+=4,HEIGHT*4/5, DIAMETER, DIAMETER);
+    }
+
+
+}
